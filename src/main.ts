6d3cfb9174e0d25912ede3d28a81c3acaea60ebb
Jeff Woo <jeff@terra.money>
2021-12-20 14:14:36 +0900

    feat: add global path prefix

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BigNumber } from 'bignumber.js';
 import axios from 'axios';
+import { RequestMethod } from '@nestjs/common';
 
 // https://mikemcl.github.io/bignumber.js/#exponential-at
 BigNumber.config({ EXPONENTIAL_AT: 1e9 });
@@ -10,6 +11,9 @@ axios.defaults.baseURL = 'https://fcd.terra.dev/v1/dashboard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('chart', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
   await app.listen(3000);
 }
 bootstrap();
